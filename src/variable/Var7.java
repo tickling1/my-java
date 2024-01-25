93f28f32f3988223257dc5851251ddda066d9f27
tickling1 <allworldreview0130@gmail.com>
2024-01-25 21:31:01 +0900

    Java(0125)

---
+++ src/variable/Var7.java
@@ -0,0 +1,27 @@
+package variable;
+/*
+리터럴(literal): 코드에서 개발자가 직접 적은 고정된 값을 프로그래밍 용어
+ex. 정수 리터럴, 실수 리터럴 등으로 불린다.
+ */
+
+public class Var7 {
+
+    public static void main(String[] args) {
+        int a = 100; //정수
+
+        double b =10.5; //실수
+
+        boolean c = true; //불리언 참or거짓 판단
+
+        char d = 'A'; //문자 하나, 작은 따옴표
+
+        String f = "Hello java"; //문자열, 문자열을 다루기 위한 특별한 타입, 큰따옴표
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+
+    }
+}
